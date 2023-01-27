4141bc6b4d7e489de4dbc22ad315837804363226
Stefan Wasmer <stefan.wasmer@tu-ilmenau.de>
2023-01-27 14:34:20 +0100

    [27.01.2023] PuA Hausaufgabe Aufgabe 4: established function add

--- src/PuAUeb11/Aufgabe4.java
+++ src/PuAUeb11/Aufgabe4.java
@@ -11,7 +11,57 @@ class BigNum {
         list = arr;
     }
 
+    public String toString() {
+        String s = "[" + list[0];
+        for (int i = 1; i < list.length; i++) {
+            s = s + ", " + list[i];
+        }
+        s = s + "]";
+        return s;
+    }
 
+    private String IntArrToString(int[] arr) {
+        String s = String.valueOf(arr[0]);
+        for (int i = 1; i < arr.length; i++) {
+            s += arr[i];
+        }
+        return s;
+    }
+
+    /*
+    public BigNum mult (BigNum x) {
+
+    }*/
+
+    public BigNum add (BigNum x) {
+        boolean carry = false;
+        int[] ergebnis = (this.list.length >= x.list.length) ? new int[this.list.length] : new int[x.list.length];
+        for (int i = ergebnis.length-1; i >= 0; --i) {
+            if (!carry) {
+                if (x.list[i]+this.list[i] <= 9) {
+                    ergebnis[i] = x.list[i]+this.list[i];
+                    carry = false;
+                } else {
+                    ergebnis[i] = x.list[i]+this.list[i] - 10;
+                    carry = true;
+                }
+            } else {
+                if (x.list[i]+this.list[i] <= 8) {
+                    ergebnis[i] = x.list[i]+this.list[i] + 1;
+                    carry = false;
+                } else {
+                    ergebnis[i] = x.list[i]+this.list[i] - 9;
+                    carry = true;
+                }
+            }
+        }
+        if (carry) {
+            return new BigNum(1 + IntArrToString(ergebnis));
+        } else {
+            return new BigNum(IntArrToString(ergebnis));
+        }
+
+    }
 
 
 }
@@ -19,6 +69,10 @@ class BigNum {
 
 public class Aufgabe4 {
     public static void main(String[] args) {
-
+        BigNum test1 = new BigNum("314");
+        BigNum test2 = new BigNum("848");
+        System.out.println(test1.toString());
+        System.out.println(test2.toString());
+        System.out.println("Add: " + test1.add(test2));
     }
 }
