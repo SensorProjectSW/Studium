c09888a9c287f31263189f33526e053ce71893c0
Stefan Wasmer <stefan.wasmer@tu-ilmenau.de>
2023-03-11 16:18:38 +0100

    [11.03.2023] PuA Aufgabe 6 WS0708 zur Probe für die Klausur

---
+++ src/Aufgabe6WS0708.java
@@ -0,0 +1,52 @@
+public class Aufgabe6WS0708 {
+    public static void main(String[] args) {
+        List list = new List();
+        List.Node zwei = new List.Node(null, 2);
+        List.Node eins = new List.Node(zwei, 4);
+        list.set(list.head, eins);
+
+
+        System.out.println(list.lessThan(3));
+        System.out.println(list.lessThanRec(3));
+    }
+}
+
+class List {
+    Node head;
+    static class Node {
+        Node next;
+        int data;
+        public Node (Node n, int s) {
+            next = n;
+            data = s;
+        }
+    }
+
+    public void set(Node alt, Node neu) {
+        alt.next = neu;
+    }
+
+    public List() {
+        head = new Node(null, -1);
+    }
+
+    public int lessThan (int c) {
+        int count = 0;
+        Node temp = head.next;
+        while (temp != null) {
+            if (temp.data < c) count++;
+            temp = temp.next;
+        }
+        return count;
+    }
+
+    public int lessThanRec(int c) {
+        return lessThanRec(c, head.next, 0);
+    }
+
+    public int lessThanRec (int c, Node temp, int i) {
+        if (temp == null) return i;
+        if (temp.data < c) return lessThanRec(c, temp.next, ++i);
+        else return lessThanRec(c, temp.next, i);
+    }
+}
